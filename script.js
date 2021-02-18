8a5bf59ab2e7d8c3df8f3ca4387e7ff10c4d4b29
ashhadimam07 <ashhadimam07@gmail.com>
2021-02-18 10:24:13 +0530

    Modify script

--- script.js
+++ script.js
@@ -9,25 +9,26 @@ let startTimer = null;
 function stopInterval() {
     clearInterval(startTimer);
 }
-function timer(){
-    if(h.value == 0 && m.value == 0 && s.value == 0){
+
+function timer() {
+    if(h.value == 0 && m.value == 0 && s.value == 0) {
         h.value = 0;
         m.value = 0;
         s.value = 0;
-    } else if(s.value != 0){
-        s.value--;
-    } else if(m.value != 0 && s.value == 0){
+    } else if(s.value != 0) {
+        s.value -= 1;
+    } else if(m.value != 0 && s.value == 0) {
         s.value = 59;
-        m.value--;
-    } else if(h.value != 0 && m.value == 0){
+        m.value -= 1;
+    } else if(h.value != 0 && m.value == 0) {
         m.value = 60;
-        h.value--;
+        h.value -= 1;
     }
     return;
 }
 
-start.addEventListener('click', function(){
-    function startInterval(){
+start.addEventListener('click', function() {
+    function startInterval() {
         startTimer = setInterval(function() {
             timer();
         }, 1000);
@@ -35,7 +36,7 @@ start.addEventListener('click', function(){
     startInterval();
 });
 
-reset.addEventListener('click', function(){
+reset.addEventListener('click', function() {
     h.value = 0;
     m.value = 0;
     s.value = 0;
